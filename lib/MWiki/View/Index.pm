9a45d0e24cd38c8ece67b24f2e4f64632fc77487
Dean Pearce <deanpearce@gmail.com>
2014-04-13 11:06:10 -0700

    Generating some starter models, views and controllers

---
+++ lib/MWiki/View/Index.pm
@@ -0,0 +1,33 @@
+package MWiki::View::Index;
+
+use strict;
+use warnings;
+
+use parent 'Catalyst::View::Mason';
+
+__PACKAGE__->config(use_match => 0);
+
+=head1 NAME
+
+MWiki::View::Index - Mason View Component for MWiki
+
+=head1 DESCRIPTION
+
+Mason View Component for MWiki
+
+=head1 SEE ALSO
+
+L<MWiki>, L<HTML::Mason>
+
+=head1 AUTHOR
+
+Dean Pearce,,,
+
+=head1 LICENSE
+
+This library is free software . You can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
+
+1;
---
+++ t/view_Index.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use Test::More;
+
+
+BEGIN { use_ok 'MWiki::View::Index' }
+
+done_testing();
